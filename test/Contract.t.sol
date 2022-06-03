ff25e147a9d3aed90f874e513bfc2205e0549d58
0xGabi <gabrielpk.18@gmail.com>
2022-06-03 00:34:58 -0300

    chore: more boilerplate code

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,23 +1,12 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-import "src/Contract.sol";
+contract ContractTest is Test {
+    function setUp() public {}
 
-contract TestContract is Test {
-    Contract c;
-
-    function setUp() public {
-        c = new Contract();
-    }
-
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
-    }
-
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
+    function testExample() public {
+        assertTrue(true);
     }
 }
